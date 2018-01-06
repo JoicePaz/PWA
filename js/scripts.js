63ddd9bbc1107c5a8566a8e57f04a8460550c783
Joice Paz <joicepazsilva@gmail.com>
2018-01-05 22:17:17 -0200

    The notes started to appear on the interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
 let notes = {data: []};
 
 let upDateList = function(){
+    console.log('[Application] start watch');
 
+    Array.observe(notes.data, function(changes){
+        let index = null;
+        let value = '';
+        let status = null;
+
+        if(changes[0].type === 'splice'){
+            index = changes[0].index;
+            value = changes[0].object[index];
+            status = (changes[0].addedCount > 0) ? 'created' : 'removed';
+        }
+
+        if(changes[0].type === 'update'){
+            index = changes[0].name;
+            value = changes[0].object[index];
+            status = 'updated';
+        }
+
+        if(value && status === 'created' && status === 'updated'){
+            return;
+        }
+
+        let notesTag = document.getElementById('notes');
+
+        if(status === 'updated'){
+            //,,,,,,,,
+        }
+
+        
+        if(status === 'removed'){
+            let listOfNotes = document.querySelectorAll("#notes li");
+            notesTag.removeChild(listOfNotes[index]);
+        }
+
+        
+        if(status === 'created'){
+            let newLi = document.createElement('li');
+            newLi.innerHTML = value;
+            notesTag.appendChild(newLi);
+        }
+    });
 }
 
 let createNote = function(){
@@ -18,6 +59,15 @@ upDateList();
 
 
 document.addEventListener('DOMContentLoaded', function(event){
+    let listOfNotes = document.getElementById('notes');
+    let listHTML = '';
+
+    for(let i=0; i < notes.data.length; i++){
+        listHTML += '<li>' + notes.data[i] + '</li>';
+    }
+
+    listOfNotes.innerHTML = listHTML;
+
     let formAddNotes = document.getElementById('form-add-note')
     formAddNotes.addEventListener('submit', function (e){
         e.preventDefault();
